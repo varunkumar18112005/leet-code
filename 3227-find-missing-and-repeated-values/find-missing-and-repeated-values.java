e46c9265b3c9dee712c2d5bee305b5f5be9255dd
Varun Bojjireddy <166485347+varunkuamr18@users.noreply.github.com>
2026-07-24 15:11:50 +0530

    Time: 1 ms (99.84%) | Memory: 47.5 MB (41.78%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-          int n = grid.length;
-        int size = n * n;
-        int[] frequency=new int[size+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<grid[i].length;j++){
-                frequency[grid[i][j]]++;
-            }
+       long actual_sum=0;
+       long squared_sum=0;
+       int n=grid.length*grid[0].length;
+       for (int i=0;i<grid.length;i++){
+        for(int j=0;j<grid[i].length;j++){
+            actual_sum+=grid[i][j];
+            squared_sum+=(long)grid[i][j]*grid[i][j];
         }
-        int repeated =0;
-        int missing=0;
-        for(int i=0;i<frequency.length;i++){
-            if(frequency[i]==2){
-                repeated=i;
-            }if(frequency[i]==0){
-                missing=i;
-            }
-        }
-        return new int[]{repeated,missing};
+       }
+       long Expected_sum=(long)(n*(n+1))/2;
+       long Expected_square_sum=(long)(n*(n+1)*(2L*n+1))/6;
+       long diff=actual_sum-Expected_sum;
+        long squareDiff = squared_sum - Expected_square_sum;
+        long sqd=squareDiff/diff;
+        int r=(int)(sqd+diff)/2;
+        int m=(int)(r-diff);
+        return new int[]{r,m};
     }
 }
