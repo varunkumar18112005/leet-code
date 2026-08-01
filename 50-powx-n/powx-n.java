94053715059990cd4c4f893b1f05ef27edc677c6
Varun Bojjireddy <166485347+varunkumar18112005@users.noreply.github.com>
2026-08-01 17:23:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (85.52%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+        double ans = 1;
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                ans*= x;
+            }
+            x *= x;
+            power /= 2;
+        }
+        return ans;
+    }
+}
